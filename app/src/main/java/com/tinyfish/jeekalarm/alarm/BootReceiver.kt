df8102a5be3557b40ae6e05b999405afa40ff902
tinyfish <tifish@gmail.com>
2020-03-09 17:37:52 +0800

    Remove redundant initialization in BootReceiver.

--- app/src/main/java/com/tinyfish/jeekalarm/alarm/BootReceiver.kt
+++ app/src/main/java/com/tinyfish/jeekalarm/alarm/BootReceiver.kt
@@ -3,12 +3,9 @@ package com.tinyfish.jeekalarm.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.tinyfish.jeekalarm.App
-import com.tinyfish.jeekalarm.schedule.ScheduleManager
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        ScheduleManager.setNextAlarm()
+        // App has already been initialized
     }
 }
