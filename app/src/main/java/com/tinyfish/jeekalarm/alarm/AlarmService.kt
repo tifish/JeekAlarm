8f6f7e1dca9f7447c539dd99caab458bee161bbd
Tinyfish <tifish@gmail.com>
2022-02-16 00:59:05 +0800

    Remove some warnings.

--- app/src/main/java/com/tinyfish/jeekalarm/alarm/AlarmService.kt
+++ app/src/main/java/com/tinyfish/jeekalarm/alarm/AlarmService.kt
@@ -4,7 +4,6 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import com.tinyfish.jeekalarm.start.App
 import java.util.*
 
@@ -18,16 +17,22 @@ object AlarmService {
 
         alarmManager = App.context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(App.context, AlarmReceiver::class.java)
-        alarmIntent = PendingIntent.getBroadcast(App.context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+        alarmIntent =
+            PendingIntent.getBroadcast(
+                App.context,
+                0,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_CANCEL_CURRENT
+            )
     }
 
     fun setAlarm(calendar: Calendar) {
         initOnce()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            alarmManager?.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, alarmIntent)
-        } else {
-            alarmManager?.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, alarmIntent)
-        }
+        alarmManager?.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            alarmIntent
+        )
     }
 
     fun cancelAlarm() {
