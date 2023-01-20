d02137fea1da69edde2665395b796306571770e0
Tinyfish <tifish@gmail.com>
2023-01-21 00:35:59 +0800

    Fix crash when alarm removed before dismissing.

--- app/src/main/java/com/tinyfish/jeekalarm/alarm/NotificationScreen.kt
+++ app/src/main/java/com/tinyfish/jeekalarm/alarm/NotificationScreen.kt
@@ -1,6 +1,13 @@
 package com.tinyfish.jeekalarm.alarm
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -35,7 +42,8 @@ fun NotificationContent() {
 
     Column {
         for (alarmId in App.notificationAlarmIds) {
-            val schedule = ScheduleService.scheduleList.filter { schedule -> schedule.id == alarmId }[0]
+            val schedule = ScheduleService.scheduleList.firstOrNull { schedule -> schedule.id == alarmId }
+                ?: continue
 
             Text(
                 schedule.name,
