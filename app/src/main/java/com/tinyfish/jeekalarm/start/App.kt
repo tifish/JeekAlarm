64ac928fce15406b10f3d16e202bfdccb6c8fc8c
Tinyfish <tifish@gmail.com>
2023-06-24 19:41:50 +0800

    Display time with more readable format.

--- app/src/main/java/com/tinyfish/jeekalarm/start/App.kt
+++ app/src/main/java/com/tinyfish/jeekalarm/start/App.kt
@@ -39,9 +39,40 @@ class App : Application() {
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
 
-        private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA)
+        private val dateFormat = SimpleDateFormat("MM-dd", Locale.US)
+        private val timeFormat = SimpleDateFormat("HH:mm", Locale.US)
+        private val weekFormat = SimpleDateFormat("EEE", Locale.US)
+
         fun format(calendar: Calendar?): String {
-            return if (calendar == null) "" else simpleDateFormat.format(calendar.time)
+            if (calendar == null)
+                return ""
+
+            val now = Calendar.getInstance()
+            val calendarYear = calendar.get(Calendar.YEAR)
+            val sameYear = now.get(Calendar.YEAR) == calendarYear
+            val today = now.get(Calendar.DAY_OF_YEAR)
+            val tomorrow = today + 1
+            val dat = tomorrow + 1
+            val calendarDay = calendar.get(Calendar.DAY_OF_YEAR)
+            val isToday = sameYear && calendarDay == today
+            val isTomorrow = sameYear && calendarDay == tomorrow
+            val isDat = sameYear && calendarDay == dat
+
+            var dateString = dateFormat.format(calendar.time)
+            if (isToday)
+                dateString = "Today"
+            else if (isTomorrow)
+                dateString = "Tmr"
+            else if (isDat)
+                dateString = "DAT"
+            else if (!sameYear)
+                dateString = "$calendarYear-$dateString"
+
+            val weekString = weekFormat.format(calendar.time)
+
+            val timeString = timeFormat.format(calendar.time)
+
+            return "$dateString $timeString $weekString"
         }
 
         var editScheduleId = -1
